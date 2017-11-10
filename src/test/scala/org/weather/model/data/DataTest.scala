d7c0ee3275cf331e0f7dfc8c231611bf52358cb2
singh-anurag <33480243+singh-anurag@users.noreply.github.com>
2017-11-10 17:46:14 +0530

    Update DataTest.scala

--- src/test/scala/org/weather/model/data/DataTest.scala
+++ src/test/scala/org/weather/model/data/DataTest.scala
@@ -8,20 +8,20 @@ class TestGetDataAndFileOperations extends FunSuite {
 
   test("Test get City to Codes Mapping") {
     //Test to verify City Name, IATACode and Bom file names could be parsed
-    val data = GetData.getMappingData.map(x => Array(x._1, x._2.IATACode, x._2.bomCode).mkString(CommonData.defaultWriteSep)).toArray
-    assert(data.sorted.deep === CommonData.cities.sorted.deep)
+    val data = GetData.getMappingData.map(x => Array(x._1, x._2.IATACode, x._2.bomCode).mkString(GenericData.defaultWriteSep)).toArray
+    assert(data.sorted.deep === GenericData.cities.sorted.deep)
   }
 
   test("File Download") {
     //Check that File for given date and bom FileName could download.
-    GetData.fileDownloader(201605, CommonData.codes("CBR", "IDCJDW2801"))
+    GetData.fileDownloader(201605, GenericData.codes("CBR", "IDCJDW2801"))
     assert(Files.exists(Paths.get("IDCJDW2801")))
   }
   
   test("Parse DateRange") {
     //Test to confirm the get max & min date parsed values is equal to default
     //min and max values.
-    val x = GetData.getMaxMinDate(CommonData.defaultDateRange)
+    val x = GetData.getMaxMinDate(GenericData.defaultDateRange)
     assert(x.min == 201505 && x.max == 201606)
   }
   
