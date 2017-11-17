ac6a77994de712f1d7d649452009f5e35125e9cc
singh-anurag <33480243+singh-anurag@users.noreply.github.com>
2017-11-17 10:58:13 +0530

    Update PredictFunctions.scala

--- src/main/scala/org/weather/model/forecast/PredictFunctions.scala
+++ src/main/scala/org/weather/model/forecast/PredictFunctions.scala
@@ -11,12 +11,10 @@ import breeze.linalg.Vector
  * @author anurag
  * @written 10 Novemebr, 2017
  * @description 
- * 			Organizes the functions used in performing the prediction functions 
- * 
+ * Organizes the functions used in performing the prediction functions 
  */
 
 object PredictFunctions {
-
   /*
    * Determines the t+1th day for which the forecast is being performed. Here, n is the max date in the observations.
    * The date is converted from Australia Date to UTC format as the time is suffixed by 'Z' in the sampled output file.
@@ -29,7 +27,6 @@ object PredictFunctions {
       .withZone(DateTimeZone.UTC)
       .toString(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z"))
   }
-
   /*
    * For the input vector, executes the ARIMA function to forecast the next value
    */
@@ -38,13 +35,10 @@ object PredictFunctions {
       .forecast(Vector(values), values.size + 1)
       .toArray
       .last
-
   }
-
   /*
    * Function to predict the outlook weather. This application classifies, the out look as Sunny, Rain, Cloudy, 
    * Cold, Partly Cloudy. 
-   * 
    */
   def predictOutlook(predictedRH: Int, predicteCloudCover: Int, predictedSunShine: Double, predictedAverageTemp: Double) = {
     //Rain. When highly humid, high cloud cover and less sunshine.
@@ -53,9 +47,7 @@ object PredictFunctions {
     else if (predicteCloudCover > 6) "Cloudy" //When high cloud cover then cloudy.
     else if (predictedAverageTemp < 15) "Cold" //If temperature is below 15. Then Cold.
     else "Partly Cloudy" //If not everything else, then "Partly Cloudy"
-
   }
-
   /*
    * Core function to execute predictions for various measurements for each of the city.
    * 1. Reads the observations & extracts required fields
@@ -71,15 +63,12 @@ object PredictFunctions {
       city <- cityToCodeMapping.keys.toArray
       val bomObservations = ReadFile.getBomObservations(cityToCodeMapping(city).bomCode)
       val tPlus1Date = PredictFunctions.getPlusOneday(bomObservations)
-
       val predictedAverageTemp = PredictFunctions.executeARIMAForecast(bomObservations.map(x => (x.max_temp + x.min_temp) / 2).toArray)
       val predictedPressure = PredictFunctions.executeARIMAForecast(bomObservations.map(x => x.pressure).toArray)
       val predictedRH = PredictFunctions.executeARIMAForecast(bomObservations.map(x => x.relative_humidity.toDouble).toArray).round.toInt
       val predictedCloudCover = bomObservations.map(x => x.cloud_amount).max
       val predictedSunShine = ((bomObservations.map(x => x.sunshine).sum) / bomObservations.size).round.toInt
-
       val outlook = PredictFunctions.predictOutlook(predictedRH, predictedCloudCover, predictedSunShine, predictedAverageTemp)
-
       val geoCordinates = GetData.getLatLong(city)
     } yield Array(cityToCodeMapping(city).IATACode, geoCordinates, tPlus1Date, outlook, "%.2f".format(predictedAverageTemp), "%.2f".format(predictedPressure), predictedRH).mkString(GenericData.defaultWriteSep)
 }
